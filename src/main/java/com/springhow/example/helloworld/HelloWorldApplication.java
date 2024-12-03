19ca8e37b051bb9162516699d1f50e2e11247c64
Chitransh-3284 <103239220+Hyghen@users.noreply.github.com>
2024-12-03 12:11:07 +0530

    Updated HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello World!";
+        return "Hii, This is my first website!";
     }
 
 }
